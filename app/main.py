8c134182f341127c967df72e7011470b171c165c
preston <prestonhall31@gmail.com>
2019-09-20 20:34:42 -0700

    Implemented Bootstrap into the layout.

--- app/main.py
+++ app/main.py
@@ -23,5 +23,10 @@ def blog():
     return render_template('blog.html')
 
 
+@app.route('/contact/')
+def contact():
+    return render_template('contact.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
